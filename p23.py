99a3786938e505d545c5c6578ca58c12a699803e
Mark G. Whitney <markwhitney@gmail.com>
2014-02-14 22:07:11 -0500

    fixed 23

--- p23.py
+++ p23.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     abundant = factoring.get_abundant(range(2, 28124))
     print len(abundant)
     pair_sums = {sum(pair) for pair in itertools.product(abundant, abundant)}
-    non_paired = set(range(2, 28124)) - pair_sums
+    non_paired = set(range(1, 28124)) - pair_sums
     print non_paired
 
     print sum(non_paired)
