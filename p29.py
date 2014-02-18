1eac48d56b3cd921408afaee3ba7721e294d653d
Mark G. Whitney <markwhitney@gmail.com>
2014-02-18 01:14:33 -0500

    +29

---
+++ p29.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+remaining_power = {}
+
+if __name__ == '__main__':
+    terms = set()
+    for a in range(2, 101):
+        for b in range(2, 101):
+            # filter out products that have already been computed at lower a
+            if a not in remaining_power or b > remaining_power[a]:
+                prod = a**b
+                remaining_power[prod] = 100 / b
+                terms.add(prod)
+    print len(terms)
