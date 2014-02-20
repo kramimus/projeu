47ccd24ad2002a8eacc7857b2ce1f6b73413b37f
Mark G. Whitney <markwhitney@gmail.com>
2014-02-20 01:18:08 -0500

    cleanup and now correct p32 soln

--- p32.py
+++ p32.py
@@ -6,22 +6,17 @@ import string
 ALL_DIGITS = '123456789'
 digit_set = set(ALL_DIGITS)
 
-found = set()
 def check_soln(a, b, prod_digits):
     prod = a * b
     prod_set = set(str(prod))
-    if len(prod_set) == len(str(prod)) and set(str(prod)) == set(prod_digits) and (a, b) not in found:
-        print a, b, prod
-        found.add((a, b))
-        found.add((b, a))
+    if len(prod_set) == len(str(prod)) and set(str(prod)) == set(prod_digits):
         return prod
     else:
         return None
 
 
 if __name__ == '__main__':
-    solns = []
-    terms_processed = set()
+    solns = set()
     for combo in itertools.combinations(ALL_DIGITS, 5):
         for perm in itertools.permutations(combo):
             remaining = digit_set - set(perm)
@@ -30,6 +25,5 @@ if __name__ == '__main__':
                 b = int(''.join(perm[i:]))
                 prod = check_soln(a, b, remaining)
                 if prod is not None:
-                    solns.append(prod)
-    print solns
+                    solns.add(prod)
     print sum(solns)
