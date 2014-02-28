b657fb8244549aa060e132340dfe2c5c6544f0b8
Mark G. Whitney <markwhitney@gmail.com>
2014-02-28 00:45:45 -0500

    brute force search

--- p38.py
+++ p38.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
 if __name__ == '__main__':
-    for i in range(int(1e10)):
-        pass
+    maxes = []
+    for i in range(int(1e6)):
+        prev = 0
+        n = 1
+        total = str(i)
+        while len(total) < 10:
+            prev = total
+            n += 1
+            total += str(n * i)
+        digits = set(prev)
+        digits.discard('0')
+        if len(digits) == 9:
+            maxes.append((int(prev), n - 1, i, len(prev)))
+    print(max(maxes))
