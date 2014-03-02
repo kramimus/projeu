8595045f9eeabe7204f76c0b932fbd72d018ac27
Mark G. Whitney <markwhitney@gmail.com>
2014-03-02 16:59:45 -0500

    got tired of bookkeeping p40 and solved the easy way with one big string

--- p40.py
+++ p40.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python3
 
-def remove_full_ranges(digit_n):
-    if digit_n < 10:
-        return 1, digit_n
-    else:
-        digit_n -= 9
+from functools import reduce
+import math
+from operator import mul
 
+def remove_full_ranges(digit_n):
+    digit_n -= 10
+    start = 10
     exp = 2
-    current_max = int(10**exp) - 1
+    current_max = int(10**exp)
     while digit_n > current_max * exp:
-        digit_n -= current_max * exp
+        print(digit_n, current_max * exp)
+        digit_n -= (current_max - start) * exp
+        start += int(10**exp)
         exp += 1
-        current_max = int(10**exp) - 1
-    return exp, digit_n
+        current_max = int(10**exp)
+    return exp, digit_n, start
+
+def easy_way(max_target):
+    digit_count = math.log(max_target, 10)
+    concated = ''
+    print(int(max_target // digit_count))
+    for i in range(1, int(max_target // (digit_count - 1))):
+        concated += str(i)
+    return concated
 
 if __name__ == '__main__':
     targets = [1, 10, 100, 1000, 10000, 100000, 1000000]
-    digits = []
+    # digits = []
+    # for target in targets:
+    #     if target < 10:
+    #         digits.append(target)
+    #         continue
+    #     print(target)
+    #     digit_count, remainder, start = remove_full_ranges(target)
+    #     number = start + remainder // digit_count
+    #     place = remainder % digit_count
+    #     print(digit_count, remainder, number, place)
+    #     digits.append(int(str(number)[place]))
+    #     print(digits)
+
+    prod = 1
+    frac_string = easy_way(targets[-1])
     for target in targets:
-        digit_count, remainder = remove_full_ranges(target)
-        print(digit_count, remainder)
-        number = remainder // digit_count
-        place = remainder % digit_count
-        digits.append(int(str(number)[place]))
-        print(digits)
+        prod *= int(frac_string[target-1])
+    print(prod)
