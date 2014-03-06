1d19b66e8684b0dfce764a98a30f691c6d22fb6e
Mark G. Whitney <markwhitney@gmail.com>
2014-03-06 10:13:36 -0500

    +44

---
+++ p44.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import itertools
+import sys
+
+def tri(i):
+    return int(i*(3*i - 1) // 2)
+
+def next_pentagonal():
+    candidate_idx = 1
+    # lookahead index for checking the p_j + p_k sums, doing some
+    # math, you can see that you only need to go about a factor of
+    # sqrt(2) in n
+    gen_ahead_idx = int(candidate_idx * 1.5)
+    tri_so_far = set()
+    j = 1
+    while True:
+        while j < gen_ahead_idx:
+            tri_so_far.add(tri(j))
+            j += 1
+
+        candidate = tri(candidate_idx)
+        for lesser_tri in tri_so_far:
+            if candidate - lesser_tri in tri_so_far and candidate + lesser_tri in tri_so_far:
+                yield (lesser_tri, candidate)
+        candidate_idx += 1
+        gen_ahead_idx = int(candidate_idx * 1.5)
+
+
+# this is a hack, this only prints diffs infinitely and the user is
+# supposed to stop it when they think it is done
+#
+# empirically, the first (smallest k,j) pentagonal pair is the one you want
+if __name__ == '__main__':
+    min_diff  = None
+    for (smaller, bigger) in next_pentagonal():
+        diff = bigger - smaller
+        if min_diff is None or diff < min_diff:
+            min_diff = diff
+            print(min_diff)
